b77a3831341f3045ab956bce30b637e736759bb4
Yang Wu <yanglu2020@gmail.com>
2026-02-04 14:02:49 -0800

    working on the main page carousel

--- unstuck-duck/app/home/page.tsx
+++ unstuck-duck/app/home/page.tsx
@@ -1,20 +1,157 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
 
 export default function Home() {
-    return (
-      <div className="flex min-h-screen items-center justify-center bg-white font-sans">
-        <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white sm:items-start">
-          <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-            
-            <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black">
-              Home page
-            </h1>
-            
-          </div>
-          <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-            
-          </div>
-        </main>
-      </div>
-    );
+  const selections: string[] = ["Duck", "Lake", "Courses"];
+  const [selected, setSelected] = useState(0);
+
+  function handleLeftClick() {
+    const newSelected = (((selected - 1) % 3) + 3) % 3;
+    if (newSelected < 0 || newSelected > 2) {
+      throw new Error(`selected ${newSelected} not in range 0 to 2!`);
+    }
+    setSelected(newSelected);
   }
-  
+
+  function handleRightClick() {
+    const newSelected = (((selected + 1) % 3) + 3) % 3;
+    if (newSelected < 0 || newSelected > 2) {
+      throw new Error(`selected ${newSelected} not in range 0 to 2!`);
+    }
+    setSelected(newSelected);
+  }
+
+  const BACK_IMG_SIZE = 150;
+  const FRONT_IMG_SIZE = 300;
+
+  function showCarousel() {
+    switch (selected) {
+      case 0:
+        return (
+          <div className="flex w-full h-3/4 flex-row justify-center space-x-4">
+            <div className="">
+              <Image
+                src="/courses.png"
+                alt="Courses Image"
+                className=""
+                width={BACK_IMG_SIZE}
+                height={BACK_IMG_SIZE}
+                draggable="false"
+                onClick={handleLeftClick}
+              />
+              <Image
+                src="/left-circle-arrow.png"
+                alt="Left Arrow"
+                className=""
+                width={BACK_IMG_SIZE}
+                height={BACK_IMG_SIZE}
+                draggable="false"
+                onClick={handleLeftClick}
+              />
+            </div>
+            <div className="">
+              <Image
+                src="/duck.png"
+                alt="Duck Image"
+                className=""
+                width={FRONT_IMG_SIZE}
+                height={FRONT_IMG_SIZE}
+                draggable="false"
+              />
+            </div>
+            <div>
+              <Image
+                src="/lake.png"
+                alt="Lake Image"
+                className=""
+                width={BACK_IMG_SIZE}
+                height={BACK_IMG_SIZE}
+                draggable="false"
+                onClick={handleRightClick}
+              />
+            </div>
+          </div>
+        );
+      case 1:
+        return (
+          <div className="flex w-full h-3/4 flex-row justify-center">
+            <Image
+              src="/duck.png"
+              alt="Duck Image"
+              className=""
+              width={300}
+              height={300}
+              draggable="false"
+              onClick={handleLeftClick}
+            />
+            <Image
+              src="/lake.png"
+              alt="Lake Image"
+              className=""
+              width={300}
+              height={300}
+              draggable="false"
+            />
+            <Image
+              src="/courses.png"
+              alt="Courses Image"
+              className=""
+              width={300}
+              height={300}
+              draggable="false"
+              onClick={handleRightClick}
+            />
+          </div>
+        );
+      default:
+        return (
+          <div className="flex w-full h-3/4 flex-row justify-center">
+            <Image
+              src="/lake.png"
+              alt="Lake Image"
+              className=""
+              width={300}
+              height={300}
+              draggable="false"
+              onClick={handleLeftClick}
+            />
+            <Image
+              src="/courses.png"
+              alt="Courses Image"
+              className=""
+              width={300}
+              height={300}
+              draggable="false"
+            />
+            <Image
+              src="/duck.png"
+              alt="Duck Image"
+              className=""
+              width={300}
+              height={300}
+              draggable="false"
+              onClick={handleRightClick}
+            />
+          </div>
+        );
+    }
+  }
+
+  return (
+    <div className="flex min-h-screen items-center justify-center bg-white font-sans text-black">
+      <main className="flex min-h-screen w-full flex-col items-center justify-center bg-white sm:items-start text-black">
+        {showCarousel()}
+        <div className="flex w-full h-1/5 flex-row justify-center">
+          <button
+            className="w-30 h-16 bg-black text-lg text-white"
+            onClick={alert}
+          >
+            {selections[selected]}
+          </button>
+        </div>
+      </main>
+    </div>
+  );
+}
